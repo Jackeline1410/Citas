57137ed8340a488e5dbfb064d6cf25e653d521f8
Jackeline <ing.jacke@gmail.com>
2021-03-09 04:57:56 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Fragment, useState } from 'react';
+import Formulario from './components/Formulario';
+
+function App() {
+// Arreglo de citas
+  const [citas, guardarCitas] = useState([]);
+
+
+// Funcion que tome las citas actuales y agregue la nueva
+  const crearCita = cita => {
+    guardarCitas([
+      ...citas,
+      cita
+    ]);
+  }
+
+  return (
+    <Fragment>
+      <h1>Administrador de Pacientes</h1>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario 
+              crearCita={crearCita}
+            />
+          </div>
+          <div className="one-half column">
+            2
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
